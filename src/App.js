833fa2e43177d050cd8addf63a6b4b11a90ce2bb
Jonathan Fields <jonathanfields@gmail.com>
2024-03-08 18:19:53 -0500

    Update App.js to include loader disappearance logic

--- src/App.js
+++ src/App.js
@@ -4,15 +4,14 @@ import { useUnityLoaded } from './unityMiddleware';
 function App() {
   const isUnityLoaded = useUnityLoaded();
 
-  // Placeholder for additional logic to be executed after Unity is loaded
-  const unityLoadedAction = () => {
-    console.log('Unity is loaded. Execute further actions here.');
-    // You can include the logic that was previously in the old app.js here
-  };
-
   React.useEffect(() => {
     if (isUnityLoaded) {
-      unityLoadedAction();
+      // Hide the loader when Unity is loaded
+      const loaderElement = document.getElementById('loader');
+      if (loaderElement) {
+        loaderElement.style.display = 'none';
+      }
+      console.log('Unity is loaded. Execute further actions here.');
     }
   }, [isUnityLoaded]);
 
@@ -20,6 +19,14 @@ function App() {
     <div className="App">
       {isUnityLoaded ? (
         <p>Unity has loaded. You can now interact with the Unity content.</p>
+      ) : (
+        <div id="loader">Loading Unity content...</div>
+      )}
+    </div>
+  );
+}
+
+export default App;
       ) : (
         <p>Loading Unity content...</p>
       )}
