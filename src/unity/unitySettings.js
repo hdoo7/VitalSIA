440421c562ae8de87d77e40a327c55312d0580bc
Jonathan Fields <jonathanfields@gmail.com>
2024-03-08 18:13:33 -0500

    Update unitySettings.js to integrate unityMiddleware

--- src/unity/unitySettings.js
+++ src/unity/unitySettings.js
@@ -1,9 +1,7 @@
 import { loadUnityScript } from './unityScriptLoader.js'
 import { EngineWebGL_u3d } from './facs/engineWebGL_u3d.js'
 import { FacsLib } from './facs/facslib.js';
-// import { MainFrame } from '../mainframe/js/mainframe.classes.js';
 import app from '../app.js'
-
 
 window.engine;
 window.facslib;
@@ -20,42 +18,37 @@ let character = {
 
 window.U3_sceneLoaded = ()=>{
     if (!unityWebGLContentLoaded) {
-        console.log("Starting fear animation sequence...")    
-        setTimeout(() => { 
-            app(engine, facslib)
-            document.getElementById("videoOverlay").classList.add("fade-out")
-        }, 50) 
-        window.unityWebGLContentLoaded = true;
+        console.log("Unity WebGL content loaded");
+        // Signal the middleware that Unity content is loaded
+        window.handleUnityLoaded();
+        unityWebGLContentLoaded = true;
     }
-}
+};
 
 window.U3_startSceneLoaded = () => {
-    console.log("Unity WebGL content loaded");
+    console.log("Unity WebGL content started loading");
 
     if (!window.unityWebGLContentLoaded) {
 
-        console.log("Unity WebGL content loaded!!!")
+        console.log("Unity WebGL content is now fully loaded!");
         facslib.load('scene_environment_simple', character.scene)
-        
-        
-        
-    
-       
+
         console.log("Prototype loaded");
-            // mainframe = new MainFrame('../mianframe/configs/eEvaConfig.xml');
-            // mainframe.run();
     }
-}
+};
 
 
 // Function to initialize Unity game instance and related settings
 function initializeUnityGame() {
-    window.gameInstance = UnityLoader.instantiate("gameContainer", character.path + "webgl.json")
+    window.gameInstance = UnityLoader.instantiate("gameContainer", character.path + "webgl.json", {onProgress: UnityProgress});
     engine = new EngineWebGL_u3d(window.gameInstance);
     facslib = new FacsLib(engine);
     engine.FacsLib = facslib;
+}
 
-       
+// Load the Unity loader script and initialize the Unity game instance once the script is loaded
+loadUnityScript(character.path+"UnityLoader.js", initializeUnityGame)
+
      
 }
 
