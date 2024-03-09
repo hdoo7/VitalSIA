950bb13921c41dd8f840676d3dd429e66537b763
Jonathan Fields <jonathanfields@gmail.com>
2024-03-08 19:59:08 -0500

    Integrate unitySettings with index.js and adjust flow

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import { ChakraProvider } from '@chakra-ui/react';
+import './unity/unitySettings'; // Import unitySettings
 import { UnityLoadProvider } from './unityMiddleware'; // Import UnityLoadProvider
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <UnityLoadProvider> {/* Wrap App with UnityLoadProvider */}
+      <UnityLoadProvider> {/* Ensure Unity load state is available throughout the app */}
         <App />
       </UnityLoadProvider>
     </ChakraProvider>
   </React.StrictMode>,
+  document.getElementById('root')
+);
   document.getElementById('root')
 );
 );
