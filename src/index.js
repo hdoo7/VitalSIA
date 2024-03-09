4be4ad146e070b35fe02d497576f00f7dba4b076
Jonathan Fields <jonathanfields@gmail.com>
2024-03-08 19:10:48 -0500

    Wrap App component with UnityLoadProvider in index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js'; // Added file extension
+import App from './App.js';
 import { ChakraProvider } from '@chakra-ui/react';
+import { UnityLoadProvider } from './unityMiddleware'; // Import UnityLoadProvider
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <UnityLoadProvider> {/* Wrap App with UnityLoadProvider */}
+        <App />
+      </UnityLoadProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
+);
 );
