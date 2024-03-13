5dc2caf489be4b1c372f6c1a251767326caf9246
Jonathan Fields <jonathanfields@gmail.com>
2024-03-13 01:30:23 -0400

    Update App.jsx to integrate faceMaker

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useUnityState } from './unityMiddleware';
 import { smile, loopRandomBlink, stickTongueOut, pullTongueIn } from './VISOS/effectors/visualizers/facialExpressions';
 import AnimationManager from './VISOS/effectors/visualizers/AnimationManager';
 import SpeachManager from './VISOS/effectors/verbalizers/SpeachManager.js'
-import labial from './labial.js'
+import faceMaker from './faceMaker.js' // Use faceMaker instead of labial
 
 function App() {
   const { isLoaded, engine, facslib } = useUnityState();
@@ -17,17 +17,16 @@ function App() {
       const speachManager = new SpeachManager(animationManager);
       speachManager.enqueueText(`Welcome to Vis facs 2024! To get started adjusting my facial expressions, just click the hamburger menu icon on the top left.`)
       loopRandomBlink(animationManager);
-      labial();
+      faceMaker(engine, facslib); // Invoke faceMaker here
     }
   }, [isLoaded, engine, facslib]);
 
   return (
     <div className="App">
       <Loader isLoading={!isLoaded} />
-      {isLoaded && (
+      {isLoaded and (
         <>
           <p>Unity has loaded. You can now interact with the Unity content.</p>
-          {/* Include the SliderDrawer component here */}
           <SliderDrawer animationManager={new AnimationManager(facslib)} />
         </>
       )}
@@ -36,3 +35,4 @@ function App() {
 }
 
 export default App;
+
