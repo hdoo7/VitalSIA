2455dc9cea53739707cdb120a2f27b0bd835c06c
Jonathan Fields <jonathanfields@gmail.com>
2024-03-16 08:30:26 -0400

    gpt4

--- src/VISOS/reconcilers/TextToGptReconciler.js
+++ src/VISOS/reconcilers/TextToGptReconciler.js
@@ -3,7 +3,7 @@ import { OpenAI } from 'openai';
 class TextToGptReconciler {
     constructor() {
         this.openai = new OpenAI({
-            apiKey: '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',
+            apiKey: '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',
             dangerouslyAllowBrowser: true // Replace with your actual API key
         });
     }
@@ -11,7 +11,7 @@ class TextToGptReconciler {
     processText(text, customPrompt) {
         const fullPrompt = `${customPrompt} "${text}"`;
         return this.openai.chat.completions.create({
-            model: "gpt-4-turbo",
+            model: "gpt-4",
             messages: [{ role: 'user', content: fullPrompt }],
             temperature: 0.7,
             max_tokens: 1500,
