3cf5a0cb8fca9ae6dd04549f71849d3a3e3d68c8
Jonathan Fields <jonathanfields@gmail.com>
2024-06-25 02:01:34 -0400

    Update craco.config.js to properly configure Webpack for necessary polyfills

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,31 @@
-// craco.config.js - For illustration, likely unnecessary for face-api.js
+const webpack = require('webpack');
+
 module.exports = {
     webpack: {
         configure: (webpackConfig) => {
-            // Example customization (usually not needed)
+            webpackConfig.resolve.fallback = {
+                buffer: require.resolve('buffer/'),
+                crypto: require.resolve('crypto-browserify'),
+                stream: require.resolve('stream-browserify'),
+                assert: require.resolve('assert/'),
+                http: require.resolve('stream-http'),
+                https: require.resolve('https-browserify'),
+                os: require.resolve('os-browserify/browser'),
+                url: require.resolve('url/'),
+                path: require.resolve('path-browserify'),
+                fs: false,
+                net: false,
+                tls: false,
+                dns: false,
+            };
+            webpackConfig.plugins.push(
+                new webpack.ProvidePlugin({
+                    process: 'process/browser',
+                    Buffer: ['buffer', 'Buffer'],
+                })
+            );
             return webpackConfig;
         },
     },
 };
+
