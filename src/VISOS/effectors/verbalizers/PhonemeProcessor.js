a1ef39190958729fa21ddef07a21ad72d4653f94
Jonathan Fields <jonathanfields@gmail.com>
2024-06-25 18:00:47 -0400

    Add PhonemeProcessor class for handling phonemes

---
+++ src/VISOS/effectors/verbalizers/PhonemeProcessor.js
@@ -0,0 +1,9 @@
+class PhonemeProcessor {
+    getPhonemes(text) {
+        // Logic to extract phonemes from text
+        // This could involve using a library like natural.js
+        return []; // Return an array of phonemes
+    }
+}
+
+export default PhonemeProcessor;
