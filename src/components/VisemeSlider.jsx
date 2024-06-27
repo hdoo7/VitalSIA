c58cd54cba3ab4d129ed2bf050e1288eaa4540a6
Jonathan Fields <jonathanfields@gmail.com>
2024-06-27 16:01:09 -0400

    Fixed import for useState in VisemeSlider component

--- src/components/VisemeSlider.jsx
+++ src/components/VisemeSlider.jsx
@@ -1,4 +1,6 @@
+import React, { useState } from 'react';
 import { Box, Slider, SliderTrack, SliderFilledTrack, SliderThumb, Tooltip, Text } from '@chakra-ui/react';
+import * as d3 from 'd3';
 import * as d3 from 'd3';
 import React from 'react';
 import { Box, Slider, SliderTrack, SliderFilledTrack, SliderThumb, Text } from '@chakra-ui/react';
